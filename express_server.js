94a8ec476ca244c4c94dd9b8536404e4c2b54ac5
Mia Tanner <mia@indygecko.com>
2022-09-21 19:29:20 -0700

    add new login route to use user and userDatabase objects

--- express_server.js
+++ express_server.js
@@ -5,9 +5,13 @@ const cookieParser = require('cookie-parser');
 const app = express();
 const PORT = 8080; // default port 8080
 
+// ----------------------- APP.USE (MIDDLEWARE):
+
 app.use(morgan('dev'));
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+
+// --------------------------------
 
 // set ejs as view engine
 app.set("view engine", "ejs");
@@ -17,10 +21,11 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
-const userDatabase = {
+const userDatabase = {};
 
-};
+//------------------------------------------------- HELPER FUNCTIONS
 
+//generates a random string made of lower or uppercase letters or digits 0-9
 const generateRandomString = function() {
   const chars = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
   let result = '';
@@ -31,10 +36,14 @@ const generateRandomString = function() {
   return result;
 };
 
+
+// finds user in userDatabase if user has matching email to the given email
+// returns the user object if user is found
+// returns null if user is not found
 const findUserByEmail = function(email) {
   for (const user in userDatabase) {
     if (userDatabase[user].email === email) {
-      return user;
+      return userDatabase[user];
     }
   }
 
@@ -42,16 +51,11 @@ const findUserByEmail = function(email) {
 };
 
 
+//--------------------------------------- APP.GET :
+
 app.get("/", (req, res) => {
-  res.send("Hello! This is a placeholder");
+  res.send("Hello! This is a placeholder for a home page!");
 });
-
-
-// redirects to long url that has id of id
-app.get("/u/:id", (req, res) => {
-  res.redirect(urlDatabase[req.params.id]);
-});
-
 
 app.get("/urls", (req, res) => {
 
@@ -61,15 +65,14 @@ app.get("/urls", (req, res) => {
   };
 
   res.render("urls_index", templateVars);
+
 });
 
-// adds submitted url to urlDatabase with id randomly generated alphanumeric string
-app.post("/urls", (req, res) => {
-
-  const short = generateRandomString();
-  urlDatabase[short] = req.body.longURL;
-  res.redirect(`/urls/${short}`); // redirect to /urls/:id
+// redirects to long url that has id of id
+app.get("/u/:id", (req, res) => {
+  res.redirect(urlDatabase[req.params.id]);
 });
+
 
 app.get("/urls/new", (req, res) => {
 
@@ -80,15 +83,54 @@ app.get("/urls/new", (req, res) => {
   res.render("urls_new", templateVars);
 });
 
-
 app.get("/urls/:id", (req, res) => {
+
   const templateVars = {
     id: req.params.id,
     longURL: urlDatabase[req.params.id],
     user: userDatabase[req.cookies["user_id"]]
   };
+
   res.render("urls_show", templateVars);
 });
+
+app.get("/:id/edit", (req, res) => {
+
+  const templateVars = {
+    id: req.params.id,
+    longURL: urlDatabase[req.params.id],
+    user: userDatabase[req.cookies["user_id"]]
+  };
+
+  res.render("urls_edit", templateVars);
+});
+
+app.get("/login", (req, res) => {
+  const templateVars = {
+    user: userDatabase[req.cookies["user_id"]]
+  };
+
+  res.render("urls_login", templateVars);
+});
+
+app.get("/register", (req, res) => {
+  const templateVars = {
+    user: userDatabase[req.cookies["user_id"]]
+  };
+  res.render("urls_register", templateVars);
+});
+
+//--------------------------------------------- APP.POST:
+
+
+// adds submitted url to urlDatabase with id randomly generated alphanumeric string
+app.post("/urls", (req, res) => {
+
+  const short = generateRandomString();
+  urlDatabase[short] = req.body.longURL;
+  res.redirect(`/urls/${short}`); // redirect to /urls/:id
+});
+
 
 // adds the id and url to the urlDatabase and redirects back to /urls
 app.post("/urls/:id", (req, res) => {
@@ -98,22 +140,12 @@ app.post("/urls/:id", (req, res) => {
 });
 
 
-
 //removes id from urlDatabase and redirects back to /urls
 app.post("/urls/:id/delete", (req, res) => {
   delete urlDatabase[req.params.id];
   res.redirect("/urls");
 });
 
-app.get("/:id/edit", (req, res) => {
-  const templateVars = {
-    id: req.params.id,
-    longURL: urlDatabase[req.params.id],
-    user: userDatabase[req.cookies["user_id"]]
-  };
-
-  res.render("urls_edit", templateVars);
-});
 
 //changes the url assigned to the id to submitted url
 app.post("/urls/:id/edit", (req, res) => {
@@ -128,44 +160,40 @@ app.post("/urls/:id/edit", (req, res) => {
 
 });
 
-app.get("/login", (req, res) => {
-  const templateVars = {
-    user: userDatabase[req.cookies["user_id"]]
-  };
-
-  res.render("urls_login", templateVars);
-});
 
 //sets submitted username as the username cookie and redirects back to /urls
 app.post("/login", (req, res) => {
-  const { username } = req.body;
-  res.cookie("username", username);
+  const { email, password } = req.body;
+
+  if ((!findUserByEmail(email)) || (findUserByEmail(email).password !== password)) {
+    return res.status(403).send("Invalid credentials");
+  }
+
+  const userId = findUserByEmail(email).id;
+
+  res.cookie("user_id", userId);
   res.redirect("/urls");
 });
 
 //clears the username cookie and redirects back to /urls
 app.post("/logout", (req, res) => {
-  res.clearCookie("username");
+
   res.clearCookie("user_id");
   res.redirect("/urls");
+
 });
 
-app.get("/register", (req, res) => {
-  const templateVars = {
-    user: userDatabase[req.cookies["user_id"]]
-  };
-  res.render("urls_register", templateVars);
-});
 
+// takes given email and password and creates a user in userDatabase and redirects to /urls
+// if email or password fields left blank, sends status 400
+// if user with the given email exists, sends status 400
 app.post("/register", (req, res) => {
   const {email, password} = req.body;
   const id = generateRandomString();
-  console.log(userDatabase);
 
   if (!email || !password) {
     return res.status(400).send("Email or password cannot be blank");
   }
-  console.log(findUserByEmail(email));
   if (findUserByEmail(email)) {
     return res.status(400).send("User with that email already exists");
   }
@@ -180,6 +208,8 @@ app.post("/register", (req, res) => {
   res.redirect("/urls");
 });
 
+// -------------------------------------------------------------
+
 app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}!`);
+  console.log(`App listening on port ${PORT}!`);
 });
